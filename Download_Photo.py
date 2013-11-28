adbb22ac6f58595d29a6d9eee7bdb5eb70ae941c
wo8553456 <273175795@qq.com>
2013-11-28 11:02:57 +0800

    delete

--- Download_Photo.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python
-# -*- coding: utf-8 -*-
-import os,urllib
-path = '/home/lijc/neunn.jpg'
-url = "http://www.cnscn.com.cn/company/skin/default/banner.jpg"
-data = urllib.urlopen(url).read()
-f = file(path,"wb")
-f.write(data)
-f.close()
--- sort_demo2.py
+++
@@ -1,3 +0,0 @@
-a = [2,55,22,11,34,15]
-print sorted(a)
-print sorted(a,reverse=True)
